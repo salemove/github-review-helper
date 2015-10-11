a1143f2aa65eeb5dc92292313edbf10f2a535f44
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2015-10-11 18:00:40 +0300

    Create local handler type to simplify error handling/logging

---
+++ handler.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+type Handler func(http.ResponseWriter, *http.Request) Response
+
+func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	response := h(w, r)
+	response.WriteResponse(w)
+}
+
+type Response interface {
+	WriteResponse(http.ResponseWriter)
+}
+
+type ErrorResponse struct {
+	Error        error
+	Code         int
+	ErrorMessage string
+}
+
+func (r ErrorResponse) WriteResponse(w http.ResponseWriter) {
+	if r.Error != nil {
+		log.Printf("%s: %v\n", r.ErrorMessage, r.Error)
+	} else {
+		log.Println(r.ErrorMessage)
+	}
+	http.Error(w, r.ErrorMessage, r.Code)
+}
+
+type SuccessResponse struct {
+	Message string
+}
+
+func (r SuccessResponse) WriteResponse(w http.ResponseWriter) {
+	w.Write([]byte(r.Message))
+}
