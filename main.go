5e9f54683ec1a4223763fb9caf21b7be030d4e76
Deiwin Sarjas <deiwin@salemove.com>
2017-02-12 00:37:21 +0200

    Start scheduled operations immediately on SIGINT
    We're using the graceful package for running the HTTP server. The
    package [describes][1] the last step of it's process with "Returns from
    the function, allowing the server to terminate." After the HTTP server
    returns, we're currently waiting for all scheduled operations to finish
    and then return from the main method.
    
    With this change, we don't block the process from dying for too long
    after the interrupt signal by listening to it and then making our best
    effort to finish gracefully and fast. Finishing gracefully (allowing the
    scheduled operations to finish, instead of unscheduling/stopping them)
    is important, because the bot is currently built so that it can only
    guarantee smooth operation if it catches and handles all the events it's
    subscribed to.
    
    The [docs][2] have the following to say about calling Notify multiple
    times, which we do here - we call it for every delayed operation:
    
    > It is allowed to call Notify multiple times with different channels
    > and the same signals: each channel receives copies of incoming signals
    > independently.
    
    [1]: https://github.com/tylerb/graceful#behaviour
    [2]: https://golang.org/pkg/os/signal/#Notify

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"sync"
 	"time"
 
@@ -191,10 +193,25 @@ func checkUserAuthorization(issueComment IssueComment, issues Issues, repositori
 }
 
 func delay(duration time.Duration, operation func() Response, asyncOperationWg *sync.WaitGroup) {
+	interruptChan := make(chan os.Signal, 1)
+	signal.Notify(interruptChan, os.Interrupt)
+
+	timer := time.NewTimer(duration)
+
 	asyncOperationWg.Add(1)
-	time.AfterFunc(duration, func() {
+	go func() {
 		defer asyncOperationWg.Done()
+		// Avoid leaking channels
+		defer signal.Stop(interruptChan)
+
+		// Block until either of the 2 channels receives.
+		select {
+		case <-interruptChan:
+			log.Println("Received an interrupt signal (SIGINT). Starting a scheduled process immediately.")
+		case <-timer.C:
+		}
+
 		response := operation()
 		handleAsyncResponse(response)
-	})
+	}()
 }
